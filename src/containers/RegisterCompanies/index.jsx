7b65fd9e33d1e49b95f1e0e0f895d336b541c02f
Anne Beatriz Andrade <annebeatrizandrade@MacBook-Air-de-Anne.local>
2024-05-09 09:47:21 -0300

    feat register partners and companies - post

--- src/containers/RegisterCompanies/index.jsx
+++ src/containers/RegisterCompanies/index.jsx
@@ -1,28 +1,68 @@
 import { useState } from "react";
 import { Container } from "./styles"
+import axios from "axios";
 
 function RegisterCompanies()  {
 
-    const [isActive, setIsActive] = useState(false); 
+    const [companiesData, setCompaniesData] = useState({
+        companyName: "",
+        collaboratorsCount: "",
+        isActive: false
+    });
 
-    const handleIsActiveChange = (e) => {
-        setIsActive(e.target.value === 'true'); 
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setCompaniesData({
+            ...companiesData,
+            [name]: value
+        });
     };
+
+
+    const handleSubmit = async () => {
+        try {
+            await axios.post('https://655cf25525b76d9884fe3153.mockapi.io/v1/external-companies', companiesData);
+            alert('Empresa cadastrada com sucesso!');
+            setCompaniesData({
+                companyName: "",
+                collaboratorsCount: "",
+                isActive: false
+            });
+        } catch (error) {
+            console.error('Erro ao cadastrar Empresa:', error);
+            alert('Erro ao cadastrar Empresa. Tente novamente.');
+        }
+    };
+
+
 
     return(
         <Container>
         <h1>Cadastrar Empresas</h1>
         <p>Preencha as informações da nova Empresa Externa Connect Hub:</p>
         <p>Insira o Nome da Empresa:</p>
-        <input />
+        <input
+         type="text"
+         name="companyName"
+         value={companiesData.companyName}
+         onChange={handleInputChange}
+        />
         <p>Informe quantos colaboradores tem na empresa:</p>
-        <input />
+        <input 
+        type="number"
+        name="collaboratorsCount"
+        value={companiesData.collaboratorsCount}
+        onChange={handleInputChange}
+        />
         <p>A empresa está ativa?</p>
-        <select value={isActive} onChange={handleIsActiveChange}>
-                    <option value={true}>Sim</option>
-                    <option value={false}>Não</option>
-                </select>
-        <button>Cadastrar</button>
+         <select
+                value={companiesData.isActive}
+                onChange={(e) => setCompaniesData({...companiesData, isActive: e.target.value === 'true'})}
+            >
+                <option value={true}>Sim</option>
+                <option value={false}>Não</option>
+            </select>
+        <button onClick={handleSubmit}>Cadastrar</button>
          </Container>
     )
     
--- src/containers/RegisterPartners/index.jsx
+++ src/containers/RegisterPartners/index.jsx
@@ -1,14 +1,41 @@
 import { useState } from "react";
 import { Container } from "./styles"
+import axios from "axios";
 
 
 
 function RegisterPartners()  {
 
-    const [isActive, setIsActive] = useState(false); 
+    const [partnerData, setPartnerData] = useState({
+        name: "",
+        urlDoc: "",
+        repositoryGit: "",
+        isActive: false
+    });
 
-    const handleIsActiveChange = (e) => {
-        setIsActive(e.target.value === 'true'); 
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setPartnerData({
+            ...partnerData,
+            [name]: value
+        });
+    };
+
+
+    const handleSubmit = async () => {
+        try {
+            await axios.post('https://644060ba792fe886a88de1b9.mockapi.io/v1/test/partners', partnerData);
+            alert('Parceiro cadastrado com sucesso!');
+            setPartnerData({
+                name: "",
+                urlDoc: "",
+                repositoryGit: "",
+                isActive: false
+            });
+        } catch (error) {
+            console.error('Erro ao cadastrar parceiro:', error);
+            alert('Erro ao cadastrar parceiro. Verifique o console para mais detalhes.');
+        }
     };
 
     return(
@@ -16,17 +43,36 @@ function RegisterPartners()  {
        <h1>Cadastrar Parceiros</h1>
        <p>Preencha as informações do novo Parceiro Connect Hub:</p>
        <p>Insira o Nome do Parceiro:</p>
-       <input />
+       <input 
+       type="text"
+       name="name"
+       value={partnerData.name}
+       onChange={handleInputChange}
+       />
        <p>Insira a URL do Parceiro:</p>
-       <input />
+       <input
+       type="text"
+       name="urlDoc"
+       value={partnerData.urlDoc}
+       onChange={handleInputChange}
+       />
        <p>Insira o repositorio Git:</p>
-       <input />
+       <input 
+        type="text"
+        name="repositoryGit"
+        value={partnerData.repositoryGit}
+        onChange={handleInputChange}
+       
+       />
        <p>A empresa está ativa?</p>
-       <select value={isActive} onChange={handleIsActiveChange}>
-                    <option value={true}>Sim</option>
-                    <option value={false}>Não</option>
-                </select>
-       <button>Cadastrar</button>
+       <select
+                value={partnerData.isActive}
+                onChange={(e) => setPartnerData({...partnerData, isActive: e.target.value === 'true'})}
+            >
+                <option value={true}>Sim</option>
+                <option value={false}>Não</option>
+            </select>
+       <button onClick={handleSubmit}>Cadastrar</button>
         </Container>
     )
     
